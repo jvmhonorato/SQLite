3691046bc66a606eaf882ab1f2c7b56547b4ec82
Victor Honorato <101150943+vituhonorato@users.noreply.github.com>
2022-08-31 20:05:47 -0300

    created condition to start data bank

--- db.js
+++ db.js
@@ -1,15 +1,49 @@
 const sqlite = require('sqlite3').verbose()
 
 //START DATABANK
-const init = databaseFile => new Promise((resolve,reject)=>{
-  const db =  new sqlite.Database( databaseFile, (err)=> {
-        if(err){
-            reject(err)
-        }else{
-           resolve(db) 
-        }
+
+const openDatabase = databaseFile => new Promise((resolve,reject)=>{
+    const db =  new sqlite.Database( databaseFile, (err)=> {
+          if(err){
+              reject(err)
+          }else{
+             resolve(db) 
+          }
+    })
   })
-})
+
+
+const init = async( databaseFile) => {
+    const db = openDatabase(databaseFile)
+    //check if databank was created
+    const exists = await query(db, `SELECT name from sqlite_master where type = 'table' and name='categories'`)
+    if(exists.length === 0){
+        await run(db,`
+        CREATE TABLE    categories (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            category TEXT
+          );
+        ` )
+        await run(db,`
+        CREATE TABLE    products (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            product TEXT,
+            price REAL
+          );
+       ` )
+        await run(db,`
+        CREATE TABLE    images (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            description TEXT,
+            url TEXT,
+            product_id INTEGER REFERENCES products(id)
+            );
+        ` )
+
+    }
+
+    return db
+}
 
 //QUERY WITH PARAMS
 const queryWithParams = (db, query, values) => new Promise((resolve,reject) => {
